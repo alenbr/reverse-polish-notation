a82c67eee58f7beefbd61ca1d789bd4a3af85609
alencar <alencar@luizalabs.com>
2021-03-26 14:35:38 -0300

    code refactoring

--- src/main/java/calculator/RPNCalculator.java
+++ src/main/java/calculator/RPNCalculator.java
@@ -8,10 +8,10 @@ public class RPNCalculator {
 
 	public int calculate(String[] tokens) {
 		Map<String, BiFunction<Integer, Integer, Integer>> ops = Map.of(
-														"+", (v1,v2) -> v2 + v1,
-														"-", (v1,v2) -> v2 - v1,
-														"/", (v1,v2) -> v2 / v1,
-														"*", (v1,v2) -> v2 * v1 );
+				"+", (v1,v2) -> v2 + v1,
+				"-", (v1,v2) -> v2 - v1,
+				"/", (v1,v2) -> v2 / v1,
+				"*", (v1,v2) -> v2 * v1 );
 
 		Stack<Integer> operands = new Stack<>();
 		for (String value : tokens) {
