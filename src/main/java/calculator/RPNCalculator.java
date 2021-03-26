0eaa8d20cd5e125543480995b10f03d18452965d
alencar <alencar@luizalabs.com>
2021-03-26 14:23:15 -0300

    refactoring code

--- src/main/java/calculator/RPNCalculator.java
+++ src/main/java/calculator/RPNCalculator.java
@@ -1,6 +1,5 @@
 package calculator;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
 import java.util.function.BiFunction;
@@ -8,21 +7,16 @@ import java.util.function.BiFunction;
 public class RPNCalculator {
 
 	public int calculate(String[] tokens) {
+		Map<String, BiFunction<Integer, Integer, Integer>> ops = Map.of(
+														"+", (v1,v2) -> v2 + v1,
+														"-", (v1,v2) -> v2 - v1,
+														"/", (v1,v2) -> v2 / v1,
+														"*", (v1,v2) -> v2 * v1 );
+
 		Stack<Integer> operands = new Stack<>();
 		for (String value : tokens) {
-			if ("+-/*".contains(value)) 
-				operands.push( oper(value).apply(operands.pop(),operands.pop()));
-			else 
-				operands.push(Integer.parseInt(value));
+			operands.push(ops.containsKey(value) ? ops.get(value).apply(operands.pop(),operands.pop()) : Integer.parseInt(value)); 
 		}
 		return operands.isEmpty() ? 0 : operands.pop();
-	}
-	private static BiFunction<Integer, Integer, Integer> oper(String op) {
-		Map<String, BiFunction<Integer, Integer, Integer>> ops = new HashMap<>();
-		ops.put("+", (v1,v2) -> v2 + v1);
-		ops.put("-", (v1,v2) -> v2 - v1);
-		ops.put("/", (v1,v2) -> v2 / v1);
-		ops.put("*", (v1,v2) -> v2 * v1);
-		return ops.get(op);
 	}
 }
